6814f1d72ed31b7fe62e7af85cf9ae077f913129
diego-saturno <d.delacal@alumnos.upm.es>
2021-02-01 01:53:41 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# automaticPoetryGenerator
+# Prototyping pre-trained models with Streamlit
+
+After installing dependencies using `pip install -r requirements.txt`, you can start the Streamlit app using `streamlit run file.py`.
+
+All demos are in the `models/` folder. Currently, we only have one demo with GPT-2 in `models/gpt_demo.py`.
+
+To start the GPT-2 demo, run `streamlit run models/gpt_demo.py`. The browser should automatically launch with the app, but if not, it can be found at http://localhost:8501/.
---
+++ models/gpt_demo.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from transformers import pipeline, set_seed
+from transformers.pipelines import TextGenerationPipeline
+
+
+class TextGenerator:
+    def __init__(self):
+        self.generator: TextGenerationPipeline
+        self.max_length = 30
+        set_seed(1)
+
+    def load_generator(self) -> None:
+        self.generator = pipeline('text-generation', model='gpt2')
+
+    def generate_text(self, starting_text: str) -> str:
+        return self.generator(starting_text,
+                              max_length=self.max_length,
+                              num_return_sequences=1)[0]['generated_text']
+
+
+@st.cache(allow_output_mutation=True)
+def instantiate_generator():
+    generator = TextGenerator()
+    generator.load_generator()
+    return generator
+
+
+if __name__ == '__main__':
+    st.title('Automatic Poetry Generator')
+    generatePoem = st.button('Click to generate poetry')
+    if generatePoem:
+        st.write('presionado')
+
+    
+    starting_text = st.text_area('Or enter your your text and let the model complete it ...')
+    generator = instantiate_generator()
+
+    if starting_text:
+        response = generator.generate_text(starting_text)
+        st.markdown(f'Completed phrase: {response}')
